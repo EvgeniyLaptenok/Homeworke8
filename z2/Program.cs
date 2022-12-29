77b0fb7cf9ca0093463986fcb6ee32a7f4f036be
evgeniy <shtorm20300@gmail.com>
2022-12-29 22:25:07 +0600

    не находит правильно меньшую сумму

---
+++ z2/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 56: Задайте двумерный массив. 
+Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = Create2DArray(m, n, 0, 5);
+Print2DArray(matrix);
+SumOfRowElements(matrix);
+
+void SumOfRowElements(int[,] matrix)
+{
+    int[] array = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        array[i] = sum;
+        Console.WriteLine(array[i]);
+    }
+    int min = 0;
+    for (int k = 0; k < array.Length - 1; k++)
+    {
+        min = array[k];
+        if(min > array[k + 1])
+        {
+            min = array[k + 1];
+        }
+    }
+    Console.WriteLine("Минимальная сумма строки: " + min);
+}
+
+int[,] Create2DArray(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
